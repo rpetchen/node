4e714ad0518e5bb55cd5b615672f1cdde3122e60
rpetchen <rpetchen@gmu.edu>
2017-07-05 22:00:00 -0400

    finished basic page

--- Twitter/routes/index.js
+++ Twitter/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const app = express()
+const moment = require('moment')
 
 const twitProfile = require('../js/twit.js')
 
@@ -13,7 +14,17 @@ twitProfile.twitProfile((err, results) => {
 		console.log(err)
 	}
 	else if(results){
-	
+		results.twitTimeline.forEach( (val) =>{
+			val.created_at = moment(val.created_at).format('MM-DD-YYYY')
+		})
+		results.twitMessages.forEach((val) =>{
+			val.created_at_time = moment(val.created_at).format("hh:mm a")
+			val.created_at = moment(val.created_at).format('MM-DD-YYYY')
+			
+		
+		})
+
+	// res.send(results.twitMessages)
 	res.render('home', results)
 }
 })
